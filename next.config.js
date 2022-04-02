bf7943b9b6bdf1100207ca8b1a1e7948ad7aa17a
itsakashtripathi <98697978+itsakashtripathi@users.noreply.github.com>
2022-04-02 14:49:50 +0530

    MongoDB Connectivity

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MONGO_URI: "mongodb+srv://admin:ReactDB1@cluster0.cvyku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  }
 }
 
 module.exports = nextConfig
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <h2>About</h2>
+    </div>
+  )
+}
+
+export default about
