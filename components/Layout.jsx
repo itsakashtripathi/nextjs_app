0a60aa57e5d50ffc89a610628978a4dbbe71d583
itsakashtripathi <98697978+itsakashtripathi@users.noreply.github.com>
2022-04-03 17:26:52 +0530

    UI/Components Setup

---
+++ components/Layout.jsx
@@ -0,0 +1,12 @@
+import Navbar from './Navbar'
+
+function Layout({children}) {
+  return (
+    <>
+        <Navbar/>
+        {children}
+    </>
+  )
+}
+
+export default Layout
---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import {MDBBtn} from 'mdb-react-ui-kit'
+
+function Navbar() {
+  return (
+    <nav className='navbar container'>
+        <Link href="/">
+            <a className='navbar-brand'>Superhero Identity</a>
+        </Link>
+        <Link href="/new">
+            <MDBBtn>New Identity</MDBBtn>
+        </Link>
+    </nav>
+  )
+}
+
+export default Navbar
